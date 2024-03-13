8ee7f6a59b1fc54278f89b248453377ed7f64d6a
Nebulætrix <40651296@live.napier.ac.uk>
2024-03-13 16:14:24 +0000

    Added Employee class

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -64,3 +64,39 @@ public class App {
         }
     }
 }
+// Method to extract Employee data
+public Employee getEmployee(int ID)
+{
+    try
+    {
+        // Create an SQL statement
+        Statement stmt = con.createStatement();
+        // Create string for SQL statement
+        String strSelect =
+                "SELECT emp_no, first_name, last_name "
+                        + "FROM employees "
+                        + "WHERE emp_no = " + ID;
+        // Execute SQL statement
+        ResultSet rset = stmt.executeQuery(strSelect);
+        // Return new employee if valid.
+        // Check one is returned
+        if (rset.next())
+        // Setting all columns to variables
+        {
+            Employee emp = new Employee();
+            emp.emp_no = rset.getInt("emp_no");
+            emp.first_name = rset.getString("first_name");
+            emp.last_name = rset.getString("last_name");
+            return emp;
+        }
+        else
+            return null;
+    }
+    // Catch any exception and provide details
+    catch (Exception e)
+    {
+        System.out.println(e.getMessage());
+        System.out.println("Failed to get employee details");
+        return null;
+    }
+}
